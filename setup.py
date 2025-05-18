d1b032a0097ae7fbe1b0db4509610d03d2090e0f
filipture <huspupuj1@outlook.com>
2025-05-18 14:49:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ APP = ['spoofer.py']
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['PIL', 'numpy', 'piexif', 'tkinterdnd2'],
+    'plist': {
+        'CFBundleIdentifier': 'com.filipturek.spoofer',
+        'CFBundleName': 'Spoofer',
+        'CFBundleShortVersionString': '1.0',
+        'CFBundleVersion': '1.0',
+        'LSUIElement': False,
+    },
 }
 
 setup(
